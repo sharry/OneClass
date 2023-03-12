2b45de1b5d6aae73b52f60fdd53bef4a3493c2fd
ElhoubeBrahim <brahim.elhoube@gmail.com>
2023-03-12 23:31:40 +0100

    Classroom CRUD endpoints

--- OneClass.Domain/DbModels/ClassRoomData.cs
+++ OneClass.Domain/DbModels/ClassRoomData.cs
@@ -1,9 +1,10 @@
 namespace OneClass.Domain.DbModels;
 
-public record ClassRoomData(
-    string Id,
-    string Title,
-    string Description,
-    string Image,
-    string TeacherId
-);
+public class ClassRoomData {
+    public string Id { get; set; }
+    public string Title { get; set; }
+    public string Description { get; set; }
+    public string Image { get; set; }
+    public string TeacherId { get; set; }
+    public string[] StudentIds { get; set; }
+}
--- OneClass.WebAPI/Classroom/ClassroomModule.cs
+++ OneClass.WebAPI/Classroom/ClassroomModule.cs
@@ -19,39 +19,127 @@ public class ClassroomModule : ICarterModule
             ) =>
             {
                 var user = await userService.GetAuthenticatedUserAsync(context, cancellationToken);
-                return Results.Ok(user);
+                var classrooms = await session
+                    .Query<ClassRoomData>()
+                    .Where(x => x.TeacherId == user.Id || x.StudentIds.Any(id => id == user.Id))
+                    .ToListAsync(cancellationToken);
+
+                return Results.Ok(classrooms);
             }
         );
 
         app.MapGet(
             "/api/classrooms/{id}",
-            (string id) =>
+            async (
+                string id,
+                IDocumentSession session,
+                HttpContext context,
+                IUserService userService,
+                CancellationToken cancellationToken
+            ) =>
             {
-                return Results.Ok($"Classroom {id}");
+                var user = await userService.GetAuthenticatedUserAsync(context, cancellationToken);
+                var classroom = await session
+                    .Query<ClassRoomData>()
+                    .FirstOrDefaultAsync(x => x.Id == id, cancellationToken);
+
+                if (classroom is null)
+                {
+                    return Results.NotFound();
+                }
+
+                if (classroom.TeacherId != user.Id && !classroom.StudentIds.Any(x => x == user.Id))
+                {
+                    return Results.Unauthorized();
+                }
+
+                return Results.Ok(classroom);
             }
         );
 
         app.MapPost(
             "/api/classrooms",
-            (ClassRoomData classRoomData) =>
+            (
+                ClassRoomData classRoomData,
+                IDocumentSession session,
+                HttpContext context,
+                IUserService userService,
+                CancellationToken cancellationToken
+            ) =>
             {
+                var user = userService.GetAuthenticatedUserAsync(context, cancellationToken).Result;
+                classRoomData.Id = Guid.NewGuid().ToString();
+                classRoomData.TeacherId = user.Id;
+                classRoomData.StudentIds = new string[0];
+
+                session.Store(classRoomData);
+                session.SaveChanges();
                 return Results.Ok(classRoomData);
             }
         );
 
         app.MapPut(
             "/api/classrooms/{id}",
-            (string id, ClassRoomData classRoomData) =>
+            (
+                string id,
+                ClassRoomData classRoomData,
+                IDocumentSession session,
+                HttpContext context,
+                IUserService userService,
+                CancellationToken cancellationToken
+            ) =>
             {
-                return Results.Ok(classRoomData);
+                var user = userService.GetAuthenticatedUserAsync(context, cancellationToken).Result;
+                var classroom = session.Query<ClassRoomData>().FirstOrDefault(x => x.Id == id);
+
+                if (classroom is null)
+                {
+                    return Results.NotFound();
+                }
+
+                if (classroom.TeacherId != user.Id)
+                {
+                    return Results.Unauthorized();
+                }
+
+                classroom.Title = classRoomData.Title;
+                classroom.Description = classRoomData.Description;
+                classroom.Image = classRoomData.Image;
+
+                session.Store(classroom);
+                session.SaveChanges();
+
+                return Results.Ok(classroom);
             }
         );
 
         app.MapDelete(
             "/api/classrooms/{id}",
-            (string id) =>
+            (
+                string id,
+                IDocumentSession session,
+                HttpContext context,
+                IUserService userService,
+                CancellationToken cancellationToken
+            ) =>
             {
-                return Results.Ok($"Classroom {id} deleted");
+                var user = userService.GetAuthenticatedUserAsync(context, cancellationToken).Result;
+                var classroom = session.Query<ClassRoomData>().FirstOrDefault(x => x.Id == id);
+
+                if (classroom is null)
+                {
+                    return Results.NotFound();
+                }
+
+                if (classroom.TeacherId != user.Id)
+                {
+                    return Results.Unauthorized();
+                }
+
+                session.Delete(classroom);
+                session.SaveChanges();
+
+                return Results.Ok();
             }
         );
     }
