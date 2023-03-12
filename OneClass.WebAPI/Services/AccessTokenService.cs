02e0900622d08919132f1fa9a1ebb850bf403ad0
sharry <y.bensadik@outlook.com>
2023-03-12 22:40:27 +0100

    feat: users photo

---
+++ OneClass.WebAPI/Services/AccessTokenService.cs
@@ -0,0 +1,14 @@
+﻿namespace OneClass.WebAPI.Services;
+
+public class AccessTokenService : IAccessTokenService
+{
+	public string GetAccessToken(HttpContext context)
+	{
+		var token = context.Request.Headers.Authorization[0];
+		if (token is null)
+		{
+			throw new Exception("Unauthorized");
+		}
+		return token.Replace("Bearer", "");
+	}
+}
