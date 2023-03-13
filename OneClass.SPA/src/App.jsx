64725cc1eef9e017be83143c3f14e83ebe7b0e0e
Abderrazzaq LAANAOUI <abderrazzaq.laanaoui@gmail.com>
2023-03-14 00:38:07 +0100

    fixing imports

--- OneClass.SPA/src/App.jsx
+++ OneClass.SPA/src/App.jsx
@@ -2,6 +2,9 @@ import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import ClassList from "./components/ClassList";
 import OneClassContext from "./context/OneClassContext";
+import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { Link, BrowserRouter, Routes, Route } from "react-router-dom";
+
 
 const queryClient = new QueryClient({
     defaultOptions: {
@@ -13,11 +16,12 @@ const queryClient = new QueryClient({
 });
 
 const App = () => {
+    const loggedUser = useState(null);
 
     return (
         <BrowserRouter>
             <QueryClientProvider client={queryClient}>
-                <OneClassContext.Provider value={adoptedPet}>
+                <OneClassContext.Provider value={loggedUser}>
                     <header>
                         <Link to="/">OneClass</Link>
                     </header>
