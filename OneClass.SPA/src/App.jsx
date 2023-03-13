a7400940405996d10429cd37293fa126d4d6c908
Abderrazzaq LAANAOUI <abderrazzaq.laanaoui@gmail.com>
2023-03-14 00:30:01 +0100

    rounting and context

--- OneClass.SPA/src/App.jsx
+++ OneClass.SPA/src/App.jsx
@@ -1,15 +1,33 @@
 import React, { useState } from "react";
 import { createRoot } from "react-dom/client";
 import ClassList from "./components/ClassList";
+import OneClassContext from "./context/OneClassContext";
 
-
+const queryClient = new QueryClient({
+    defaultOptions: {
+        queries: {
+            staleTime: 100000,
+            cacheTime: 100000,
+        },
+    },
+});
 
 const App = () => {
 
     return (
-        <>
-        <ClassList />
-        </>
+        <BrowserRouter>
+            <QueryClientProvider client={queryClient}>
+                <OneClassContext.Provider value={adoptedPet}>
+                    <header>
+                        <Link to="/">OneClass</Link>
+                    </header>
+                    <Routes>
+                        <Route path="/classes" element={<Details />} />
+                        <Route path="/assignements" element={<SearchParams />} />
+                    </Routes>
+                </OneClassContext.Provider>
+            </QueryClientProvider>
+        </BrowserRouter>
     )
 };
 const container = document.getElementById("root");
