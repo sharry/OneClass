15dec2642041ef585bd71b339eb89eedc7316ed6
sharry <y.bensadik@outlook.com>
2023-03-14 01:21:07 +0100

    feat: auth sign in

--- OneClass.SPA/src/App.jsx
+++ OneClass.SPA/src/App.jsx
@@ -11,30 +11,35 @@ import { msalConfig } from "./config/msalConfig";
 const msalInstance = new PublicClientApplication(msalConfig);
 
 const queryClient = new QueryClient({
-    defaultOptions: {
-        queries: {
-            staleTime: 100000,
-            cacheTime: 100000,
-        },
-    },
+	defaultOptions: {
+		queries: {
+			staleTime: 100000,
+			cacheTime: 100000,
+		},
+	},
 });
 
 const App = () => {
-    const loggedUser = useState(null);
+	const loggedUser = useState(null);
 
-    return (
-        <BrowserRouter>
-            <QueryClientProvider client={queryClient}>
-                <OneClassContext.Provider value={loggedUser}>
-                    <heOneClassContexter>
-                    </heOneClassContexter>
-                    <Routes>
-                        <Route path="/" element={<Home/>}/>
-                    </Routes>
-                </OneClassContext.Provider>
-            </QueryClientProvider>
-        </BrowserRouter>
-    )
+	return (
+		<React.StrictMode>
+			<MsalProvider instance={msalInstance}>
+				<BrowserRouter>
+					<QueryClientProvider client={queryClient}>
+						<OneClassContext.Provider value={loggedUser}>
+							<heOneClassContexter>
+								<Link to="/">OneClass</Link>
+							</heOneClassContexter>
+							<Routes>
+								<Route path="/" element={<Home />} />
+							</Routes>
+						</OneClassContext.Provider>
+					</QueryClientProvider>
+				</BrowserRouter>
+			</MsalProvider>
+		</React.StrictMode>
+	)
 };
 const container = document.getElementById("root");
 const root = createRoot(container);
