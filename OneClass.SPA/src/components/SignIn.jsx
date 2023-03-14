7dd68ec0006d9fb5af8b0a2fff350780e5cd3b8e
sharry <y.bensadik@outlook.com>
2023-03-14 01:53:59 +0100

    feat: auth configured auth

--- OneClass.SPA/src/components/SignIn.jsx
+++ OneClass.SPA/src/components/SignIn.jsx
@@ -1,12 +1,10 @@
 import React from "react";
 import { SignInButton } from "./SignInButton";
-import { useIsAuthenticated } from "@azure/msal-react";
 export const SignIn = () => {
-	const isAuthenticated = useIsAuthenticated();
 	return (
 		<div>
 			<h1>Sign In</h1>
-			{ isAuthenticated ? <span>Signed In</span> : <SignInButton loginType="popup" /> }
+			<SignInButton loginType="popup" />
 		</div>
 	);
 }
--- OneClass.SPA/src/pages/Home.jsx
+++ OneClass.SPA/src/pages/Home.jsx
@@ -1,14 +1,24 @@
+import React from "react";
 import Navbar from "../components/Navbar";
 import CourseCard from "../components/CourseCard";
 import BottomTabs from "../components/BottomTabs"
+import { useIsAuthenticated } from "@azure/msal-react";
+import { SignIn } from "../components/SignIn";
 
 function Home() {
-  return (
-    <div className="">
-      <Navbar /> <CourseCard /> <CourseCard /> <CourseCard /><CourseCard />
-      <BottomTabs/>
-    </div>
-  );
+	const isAuthenticated = useIsAuthenticated();
+	return (
+		<div className="">
+			{
+				isAuthenticated ?
+				<>
+					<Navbar /> <CourseCard /> <CourseCard /> <CourseCard /><CourseCard />
+					<BottomTabs/>
+				</>
+				: <SignIn />
+			}
+		</div>
+	);
 }
 
 export default Home;
